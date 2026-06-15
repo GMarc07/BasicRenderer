9929a09f2d76e9bd3654e328a52a830f6acbaac4
Giorgio <giorgiolauriola789@gmail.com>
2026-06-15 14:22:08 +0100

    Initial setup for a basic window

--- renderer-test1/src/main/java/com/giorgio/App.java
+++ renderer-test1/src/main/java/com/giorgio/App.java
@@ -1,9 +1,26 @@
 package com.giorgio;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("/com/giorgio/AppUI.fxml"));
+
+        Scene scene = new Scene((Parent) loader.load(), 800, 600);
+
+        stage.setTitle("My JavaFX App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
