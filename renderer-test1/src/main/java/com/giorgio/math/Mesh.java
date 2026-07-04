159ea6d05d727d78a2e535aca5573e4229544f6f
Giorgio <giorgiolauriola789@gmail.com>
2026-07-04 16:35:44 +0100

    expanded mesh class, set/get pos and get triangles

--- renderer-test1/src/main/java/com/giorgio/math/Mesh.java
+++ renderer-test1/src/main/java/com/giorgio/math/Mesh.java
@@ -2,9 +2,30 @@ package com.giorgio.math;
 import java.util.List;
 
 public class Mesh {
-    List<Triangle> triangles;
+    private List<Triangle> triangles;
+
+    private vector3 position; // this is the position of the mesh in the 3d space.
 
     public Mesh(List<Triangle> newMesh){
         this.triangles = newMesh;
+        this.position = new vector3(0.0,0.0,0.0);
+    }
+
+    public Mesh(List<Triangle> newMesh, vector3 pos){
+        this.triangles = newMesh;
+        this.position = pos;
+    }
+
+
+    public void setPosition(vector3 newPos){
+        this.position = newPos;
+    }
+
+    public vector3 getPosition(){
+        return this.position;
+    }
+
+    public List<Triangle> getTriangles(){
+        return this.triangles;
     }
 }
