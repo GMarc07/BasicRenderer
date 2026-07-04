b40b40d045352ce49dcbf700a74a0bbd454e031b
Giorgio <giorgiolauriola789@gmail.com>
2026-07-04 16:35:11 +0100

    added return funcs for triange class

--- renderer-test1/src/main/java/com/giorgio/math/Triangle.java
+++ renderer-test1/src/main/java/com/giorgio/math/Triangle.java
@@ -3,7 +3,7 @@ package com.giorgio.math;
 public class Triangle {
     Vertex v0, v1, v2;
 
-    Triangle(Vertex v0, Vertex v1, Vertex v2) {
+    public Triangle(Vertex v0, Vertex v1, Vertex v2) {
         this.v0 = v0; this.v1 = v1; this.v2 = v2;
     }
 
@@ -12,4 +12,8 @@ public class Triangle {
         vector3 edge2 = v2.position.subtract(v0.position);
         return edge1.cross(edge2).normalise();
     }
+
+    public Vertex getV0() { return v0; }
+    public Vertex getV1() { return v1; }
+    public Vertex getV2() { return v2; }
 }
