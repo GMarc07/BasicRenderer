9d367ba04f6c16e94dc6c72f96778dc3c1c45047
Giorgio <giorgiolauriola789@gmail.com>
2026-08-03 15:21:50 +0100

    Added function to find AABB of mesh

--- renderer-test1/src/main/java/com/giorgio/math/Mesh.java
+++ renderer-test1/src/main/java/com/giorgio/math/Mesh.java
@@ -1,5 +1,6 @@
 package com.giorgio.math;
 import java.util.List;
+import com.giorgio.Engine.Collision.collisionDetection.Bounds;
 
 public class Mesh {
     private List<Triangle> triangles;
@@ -18,6 +19,37 @@ public class Mesh {
         this.position = pos;
     }
 
+    public Bounds getAABB(){
+        if (triangles == null || triangles.isEmpty()) {
+            return new Bounds(position, position);
+        }   
+        double minX = Double.POSITIVE_INFINITY;
+        double minY = Double.POSITIVE_INFINITY;
+        double minZ = Double.POSITIVE_INFINITY;
+    
+        double maxX = Double.NEGATIVE_INFINITY;
+        double maxY = Double.NEGATIVE_INFINITY;
+        double maxZ = Double.NEGATIVE_INFINITY;
+
+        for (Triangle triangle : triangles){
+
+            vector3[] vertices = {triangle.getV0().position, triangle.getV1().position, triangle.getV2().position};
+
+            for (vector3 v : vertices) {
+                minX = Math.min(minX, v.x+position.x);
+                minY = Math.min(minY, v.y+position.y);
+                minZ = Math.min(minZ, v.z+position.z);
+
+                maxX = Math.max(maxX, v.x+position.x);
+                maxY = Math.max(maxY, v.y+position.y);
+                maxZ = Math.max(maxZ, v.z+position.z);
+            }
+        }
+
+        return  new Bounds(new vector3(minX,minY,minZ),new vector3(maxX,maxY,maxZ));
+
+    }
+        
     public int getColour(){
         return colour;
     }
